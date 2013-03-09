c8e8b34c2e35e8f3e2b21c5d0d6531f8cd4e60de
dharmesh <cosc480@cosc480.(none)>
2013-03-08 20:27:01 -0500

    added some code

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -3,7 +3,7 @@
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
-    # you should arrange to add that movie to the database here.
+    # you should arrange to add that movie to the database here.#
     new_movie = Movie.create!(movie)
   end
   #flunk "Unimplemented"
